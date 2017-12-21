711ef2db2804ba2825c887f90b83d019473a4544
Anna Zaborowska <anna.zaborowska@cern.ch>
2017-12-21 14:48:46 +0100

    Recover content of sendpy

--- send.py
+++ send.py
@@ -1,3 +1,7 @@
+path_to_INIT = '/cvmfs/fcc.cern.ch/sw/0.8.3/init_fcc_stack.sh'
+path_to_LHE = '/afs/cern.ch/work/h/helsens/public/FCCsoft/FlatGunLHEventProducer/'
+path_to_FCCSW = '/cvmfs/fcc.cern.ch/sw/0.8.3/fccsw/0.8.3/x86_64-slc6-gcc62-opt/'
+
 import glob, os, sys,subprocess,cPickle
 import commands
 import time
@@ -79,7 +83,7 @@ if __name__=="__main__":
     simparser.add_argument('-n','--numEvents', type=int, help='Number of simulation events per job', required=True)
     simparser.add_argument('-N','--numJobs', type=int, default = 10, help='Number of jobs to submit')
     simparser.add_argument('--jobOptions', type=str, default = 'config/geantSim.py', help='Name of the job options run by FCCSW (default config/geantSim.py')
-    simparser.add_argument('-o','--output', type=str, help='Path of the output on EOS', required=True)
+    simparser.add_argument('-o','--output', type=str, help='Path of the output on EOS', default="/eos/experiment/fcc/hh/simulation/samples/")
     simparser.add_argument('-l','--log', type=str, help='Path of the logs', default = "BatchOutputs/")
 
     genTypeGroup = simparser.add_mutually_exclusive_group(required = True) # Type of events to generate
@@ -173,6 +177,7 @@ if __name__=="__main__":
             print "jet pt: ", pt
             job_dir += "etaTo1.5/" + str(pt) + "GeV/"
         else:
+            LHE = False
             job_dir += "etaFull/"
         print "=================================="
     if simargs.condor:
@@ -182,7 +187,6 @@ if __name__=="__main__":
         queue = simargs.queue
         print "queue: ", queue
 
-    produceLHE = simargs.LHE
     rundir = os.getcwd()
     nbjobsSub=0
 
@@ -193,86 +197,125 @@ if __name__=="__main__":
     # first make sure the output path exists
     current_dir = os.getcwd()
     logdir = simargs.log if os.path.isabs(simargs.log) else current_dir + '/' +  simargs.log
+    job_options = job_options if os.path.isabs(job_options) else current_dir + '/' +  job_options
     print  "Saving the logs in ... ",logdir
-    os.system("mkdir -p %s/out"%logdir)
-    os.system("mkdir -p %s/log"%logdir)
-    os.system("mkdir -p %s/err"%logdir)
 
-    lhe_dict = {"ljets":"PythiaCards/pythia_pp_LHE.cmd",
-                 "top":"PythiaCards/pythia_pp_LHE.cmd",
-                 "Wqq":"PythiaCards/pythia_pp_LHE.cmd",
-                 "Zqq":"PythiaCards/pythia_pp_LHE.cmd",
-                 "Hbb":"PythiaCards/pythia_pp_LHE.cmd"}
     # if Pythia used, figure out the Pythia card
     if simargs.physics:
-        if produceLHE:
+        if LHE:
             card = current_dir + "/PythiaCards/pythia_pp_LHE.cmd"
         else:
             card = current_dir + "/PythiaCards/pythia_pp_" + process + ".cmd"
-    print card
-    print os.path.isfile(card)
+        print card
+        print os.path.isfile(card)
 
     for i in xrange(num_jobs):
         seed = int(datetime.utcnow().strftime('%Y%m%d%H%M%S%f')[:-3])
         uniqueID='%s_%i'%(user,seed)
         print uniqueID
 
-        # frunname = 'job_%s_%s.sh'%(cardname.replace('.cmd',''),uniqueID)
-        # outfile = 'output_%s.root'%(uniqueID)
+        outfile = 'output_%s.root'%(uniqueID)
+        print "Name of the output file: ", outfile
+
+        if simargs.physics:
+            frunname = 'job_%s_%s.sh'%(process,uniqueID)
         # outfileconv = outfile.replace('.root','_converted.root')
+        else:
+            frunname = 'job_%s_%dGeV_eta%s_%s.sh'%(particle_human_names[pdg],energy,str(decimal.Decimal(str(etaMax)).normalize()),uniqueID)
 
-        # frun = None
-        # try:
-        #     frun = open(logdir+'/'+frunname, 'w')
-        # except IOError as e:
-        #     print "I/O error({0}): {1}".format(e.errno, e.strerror)
-        #     time.sleep(10)
-        #     frun = open(logdir+'/'+frunname, 'w')
+        os.system("mkdir -p %s"%logdir)
+        frun = None
+        try:
+            frun = open(logdir+'/'+frunname, 'w')
+        except IOError as e:
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+            time.sleep(10)
+            frun = open(logdir+'/'+frunname, 'w')
+        print frun
 
 
-        # commands.getstatusoutput('chmod 777 %s/%s'%(logdir,frunname))
-        # frun.write('#!/bin/bash\n')
-        # frun.write('unset LD_LIBRARY_PATH\n')
-        # frun.write('unset PYTHONHOME\n')
-        # frun.write('unset PYTHONPATH\n')
-        # frun.write('export JOBDIR=$PWD\n')
-        # frun.write('cd /afs/cern.ch/user/h/helsens/FCCsoft/Calo/FCCSW/\n')
-        # frun.write('source ./init.sh\n')
-        # frun.write('export EOS_MGM_URL=\"root://eospublic.cern.ch\"\n')
+        commands.getstatusoutput('chmod 777 %s/%s'%(logdir,frunname))
+        frun.write('#!/bin/bash\n')
+        frun.write('unset LD_LIBRARY_PATH\n')
+        frun.write('unset PYTHONHOME\n')
+        frun.write('unset PYTHONPATH\n')
+        frun.write('export JOBDIR=$PWD\n')
+        frun.write('source %s\n' % (path_to_INIT))
 
-        # if produceLHE:
-        #     frun.write('cd /afs/cern.ch/user/h/helsens/FCCsoft/Generators/FlatGunLHEventProducer\n')
+        # set options to run FCCSW
+        common_fccsw_command = './run fccrun.py %s --outName $JOBDIR/%s --numEvents %i --seed %i'%(job_options, outfile ,num_events, seed)
+        if not magnetic_field:
+            common_fccsw_command += ' --bFieldOff'
+        print '-------------------------------------'
+        print common_fccsw_command
+        print '-------------------------------------'
+        if simargs.physics:
+            if LHE:
+                frun.write('cd %s\n' %(path_to_LHE))
+                if process=='ljets':
+                    frun.write('python flatGunLHEventProducer.py   --pdg 1 2 3   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.5   --etamax 1.5  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
+                # elif process=='bjets':
+                #     frun.write('python flatGunLHEventProducer.py   --pdg 5   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.5   --etamax 1.5  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
+                elif process=='top':
+                    frun.write('python flatGunLHEventProducer.py   --pdg 6   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.5   --etamax 1.5  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
+                elif process=='Zqq':
+                    frun.write('python flatGunLHEventProducer.py   --pdg 23   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.5   --etamax 1.5  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
+                elif process=='Wqq':
+                    frun.write('python flatGunLHEventProducer.py   --pdg 24   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.5   --etamax 1.5  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
+                elif process=='Hbb':
+                    frun.write('python flatGunLHEventProducer.py   --pdg 25   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.5   --etamax 1.5  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
+                else:
+                    print 'process does not exists, exit'
+                    sys.exit(3)
+                frun.write('gunzip $JOBDIR/events.lhe.gz\n')
+                frun.write('cp %s $JOBDIR/card.cmd\n'%(card))
+                frun.write('echo "Beams:LHEF = $JOBDIR/events.lhe" >> $JOBDIR/card.cmd\n')
+                frun.write('cd %s\n' %(path_to_FCCSW))
+            # run FCCSW using Pythia as generator
+            frun.write('cd %s\n' %(path_to_FCCSW))
+            frun.write('%s  --pythia --card $JOBDIR/card.cmd \n'%(common_fccsw_command))
+        else:
+            # run single particles
+            frun.write('cd %s\n' %(path_to_FCCSW))
+            frun.write('%s  --singlePart --particle %i -e %i --etaMin %f --etaMax %f --phiMin %f --phiMax %f\n'%(common_fccsw_command, pdg, energy, etaMin, etaMax, phiMin, phiMax))
+        frun.write('cd %s\n' %(current_dir))
+        frun.write('python eoscopy.py $JOBDIR/%s %s\n'%(outfile,outdir))
 
-        #     if process=='ljets':
-        #         frun.write('python flatGunLHEventProducer.py   --pdg 1 2 3   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.4   --etamax 1.4  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
-        #     elif process=='bjets':
-        #         frun.write('python flatGunLHEventProducer.py   --pdg 5   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.4   --etamax 1.4  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
-        #     elif process=='top':
-        #         frun.write('python flatGunLHEventProducer.py   --pdg 6   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.4   --etamax 1.4  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
-        #     elif process=='Z':
-        #         frun.write('python flatGunLHEventProducer.py   --pdg 23   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.4   --etamax 1.4  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
-        #     elif process=='W':
-        #         frun.write('python flatGunLHEventProducer.py   --pdg 24   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.4   --etamax 1.4  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
-        #     elif process=='H':
-        #         frun.write('python flatGunLHEventProducer.py   --pdg 25   --guntype pt   --nevts %i   --ecm 100000.   --pmin %f   --pmax %f   --etamin -1.4   --etamax 1.4  --seed %i   --output $JOBDIR/events.lhe\n'%(num_events,pt,pt,seed))
-        #     else:
-        #         print 'process does not exists, exit'
-        #         sys.exit(3)
-        #     frun.write('gunzip $JOBDIR/events.lhe.gz\n')
-        #     frun.write('cp %s $JOBDIR/card.cmd\n'%(card))
-        #     frun.write('echo "Beams:LHEF = $JOBDIR/events.lhe" >> $JOBDIR/card.cmd\n')
-        #     frun.write('cd /afs/cern.ch/user/h/helsens/FCCsoft/Calo/FCCSW/\n')
-        #     frun.write('./run fccrun.py  %s  --outputfile=$JOBDIR/%s --nevents=%i --inputfile=$JOBDIR/card.cmd\n'%(job_options, outfile ,num_events))
+        if simargs.lsf:
+            cmdBatch="bsub -M 4000000 -R \"pool=40000\" -q %s -cwd%s %s" %(queue, logdir,logdir+'/'+frunname)
+            batchid=-1
+            #job,batchid=SubmitToLsf(cmdBatch,10)
+        else:
+            os.system("mkdir -p %s/out"%logdir)
+            os.system("mkdir -p %s/log"%logdir)
+            os.system("mkdir -p %s/err"%logdir)
+            # create also .sub file here
+            fsubname = frunname.replace('.sh','.sub')
+            fsub = None
+            try:
+                fsub = open(logdir+'/'+fsubname, 'w')
+            except IOError as e:
+                print "I/O error({0}): {1}".format(e.errno, e.strerror)
+                time.sleep(10)
+                fsub = open(logdir+'/'+fsubname, 'w')
+            print fsub
+            commands.getstatusoutput('chmod 777 %s/%s'%(logdir,fsubname))
+            fsub.write('executable            = %s/%s\n' %(logdir,frunname))
+            fsub.write('arguments             = $(ClusterID) $(ProcId)\n')
+            fsub.write('output                = %s/out/job.%s.$(ClusterId).$(ProcId).out\n'%(logdir,uniqueID))
+            fsub.write('log                   = %s/log/job.%s.$(ClusterId).log\n'%(logdir,uniqueID))
+            fsub.write('error                 = %s/err/job.%s.$(ClusterId).$(ProcId).err\n'%(logdir,uniqueID))
+            if simargs.physics:
+                fsub.write('RequestCpus = 8\n')
+            else:
+                fsub.write('RequestCpus = 4\n')
+            fsub.write('+JobFlavour = "testmatch"\n')
+            fsub.write('queue\n')
 
-        # else:
-        #     frun.write('./run fccrun.py  %s  --outputfile=$JOBDIR/%s --nevents=%i --inputfile=%s\n'%(job_options, outfile ,num_events, card))
-
-        # frun.write('cd /afs/cern.ch/user/h/helsens/FCCsoft/Calo/FCCSW/FCCSimJobs/\n')
-        # frun.write('python eoscopy.py $JOBDIR/%s %s\n'%(outfile,output_path))
-
-        # cmdBatch="bsub -M 4000000 -R \"pool=40000\" -q %s -cwd%s %s" %(queue, logdir,logdir+'/'+frunname)
-        # batchid=-1
-        # job,batchid=SubmitToBatch(cmdBatch,10)
-        # nbjobsSub+=job
+            cmdBatch="condor_submit %s/%s\n"%(logdir,fsubname)
+            print cmdBatch
+            batchid=-1
+            # job,batchid=SubmitToCondor(cmdBatch,10)
+        #nbjobsSub+=job
 
     print 'succesfully sent %i  jobs'%nbjobsSub
