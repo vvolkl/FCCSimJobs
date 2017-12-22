7851510779b9dfe6d0b5700d40b9c67986137bec
clementhelsens <clement.helsens@cern.ch>
2017-12-22 12:42:29 +0100

    add cleaner

---
+++ cleaner.py
@@ -0,0 +1,20 @@
+import json
+import os
+dicname='/afs/cern.ch/work/h/helsens/public/FCCDicts/SimulationDict_v01_test.json'
+mydict=None
+with open(dicname) as f:
+    mydict = json.load(f)
+user=os.environ['USER']
+
+for s in mydict:
+    for j in mydict[s]:
+        if j['status']=='DONE':
+            continue
+        elif j['status']=='BAD' and user in j['out']: 
+            if not os.path.exists(j['out']):
+                print 'BAD job already removed from eos   %s'%j['out']
+            else:
+                print  'remove BAD job from eos   %s'%j['out']
+                os.system('rm %s'%j['out'])
+
+            
