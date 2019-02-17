5a2eb3509f50876a93291ccc59a526035082c336
Valentin Volkl <valentin.volkl@cern.ch>
2019-02-17 22:03:26 +0100

    up

--- test/test.py
+++ test/test.py
@@ -12,17 +12,18 @@ from FCCBase.conf_10_particlegun import *
 # pick  and choose modules
 #from FCCBase.conf_20_det import *
 #from FCCBase.conf_30_sim import *
-from FCCBase.conf_32_sim_randomseed import randomEngine
-#from FCCBase.conf_99_ouput import *
+#from FCCBase.conf_32_sim_randomseed import randomEngine
+from FCCBase.conf_99_ouput import *
 
 # change a single options
-randomEngine.Seeds = [4444]
+#randomEngine.Seeds = [4444]
 # changing properties of private tools is slightly awkward
 # important to do it via the algorithm that owns it
-GenAlg().SignalProvider.PdgCodes = [14]
+GenAlg().SignalProvider.PdgCodes = [15] 
+GenAlg().SignalProvider.EtaMin = "-4.0" 
 
 # set number of Events
-ApplicationMgr().EvtMax = 12
+ApplicationMgr().EvtMax = "12"
 ApplicationMgr().OutputLevel = INFO
 genfilter.OutputLevel =INFO
 
