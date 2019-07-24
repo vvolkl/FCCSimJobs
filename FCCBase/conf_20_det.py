6a7ee2c3cc7c2e3d5e81bc52b808177ebd70effa
Valentin Volkl <valentin.volkl@cern.ch>
2019-07-24 11:15:58 +0200

    Fix detector xml path

--- FCCBase/conf_20_det.py
+++ FCCBase/conf_20_det.py
@@ -3,25 +3,14 @@
 import os
 from Gaudi.Configuration import *
 
-path_to_detector = os.environ("FCCDETECTORS") + '/'
 
-
+path_to_detector = os.environ.get("FCC_DETECTOR_XML", "")
 detectors_to_use=[
-                    path_to_detector+'Detector/DetFCChhBaseline1/compact/FCChh_DectEmptyMaster.xml',
-                    path_to_detector+'Detector/DetFCChhTrackerTkLayout/compact/Tracker.xml',
-                    path_to_detector+'/Detector/DetFCChhECalInclined/compact/FCChh_ECalBarrel_withCryostat.xml',
-                    path_to_detector+'/Detector/DetFCChhHCalTile/compact/FCChh_HCalBarrel_TileCal.xml',
-                    path_to_detector+'/Detector/DetFCChhHCalTile/compact/FCChh_HCalExtendedBarrel_TileCal.xml',
-                    path_to_detector+'/Detector/DetFCChhCalDiscs/compact/Endcaps_coneCryo.xml',
-                    path_to_detector+'/Detector/DetFCChhCalDiscs/compact/Forward_coneCryo.xml',
-                    path_to_detector+'/Detector/DetFCChhTailCatcher/compact/FCChh_TailCatcher.xml',
-                    path_to_detector+'/Detector/DetFCChhBaseline1/compact/FCChh_Solenoids.xml',
-                    path_to_detector+'/Detector/DetFCChhBaseline1/compact/FCChh_Shielding.xml',
+                    'Detector/DetFCChhBaseline1/compact/FCChh_DectMaster.xml',
                   ]
-
 from Configurables import GeoSvc
 geoservice = GeoSvc("GeoSvc")
-geoservice.detectors = detectors_to_use
+geoservice.detectors = [os.path.join(path_to_detector, _det) for _det in detectors_to_use]
 geoservice.OutputLevel = WARNING
 ApplicationMgr().ExtSvc += [geoservice]
 
